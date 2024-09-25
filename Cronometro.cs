0e233c0cd6cd2f04955800a0ce90b4243fc8f842
samuquinha444 <samuelsoaressantos384@gmail.com>
2024-09-25 16:48:24 -0300

    cronometro-simples-

---
+++ Cronometro.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        return1:
+        Console.Write("Quantos segundos o cronometro deve contar? ");
+        if(int.TryParse(Console.ReadLine(), out int seconds))
+        {
+            Console.Clear();
+            for(int i = seconds; i >= 0; i--)
+            {
+                Console.WriteLine($"Tempo restante: {i} segundos");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido!"); 
+            goto return1;
+        }
+    }
+}
